d9a787016014d237ade2e7f3523aafbec1a51aca
Manish Meganathan <757manish@gmail.com>
2021-05-22 16:59:34 +0530

    Update the smart contract to support addition of tasks
    - Added a struct definition for a generic Task object
    - Added a mapping of task id to Task objects to store the tasks of the todo list
    - Added a constructor for the contract
    - Added a function to create new tasks and add it to mapping given a task message.

--- contracts/Todolist.sol
+++ contracts/Todolist.sol
@@ -2,5 +2,28 @@
 pragma solidity ^0.5.0;
 
 contract Todolist {
-    uint public taskCount = 0;
+    // An integer that represents the number of tasks in the todo list.
+    uint public taskcount = 0;
+
+    // A struct definition of a generic task
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    // A mapping of the task id to the Task object that represents the todo list.
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask("buy more ethereum!");
+    }
+
+    // A function that creates a new Task object and assigns it to the mapping.
+    function createTask(string memory _content) public {
+        // Increment the taskcount state
+        taskcount++;
+        // Assign the new task to the tasks mapping
+        tasks[taskcount] = Task(taskcount, _content, false);
+    }
 }
