bfff3007b182b36f326b0973589c9831bf7d3c1e
Manish Meganathan <757manish@gmail.com>
2021-05-26 21:12:13 +0530

    Update the smart contract and build artifacts
    Updated the Solidity smart contract with following
    - Added events for task creation and task completion
    - Added a function to toggle the completion of tasks and emit the event.
    - Refactored task creation function to emit the event as well.
    
    Updated the contract build artifacts for the new version of the smart contract

--- contracts/Todolist.sol
+++ contracts/Todolist.sol
@@ -15,6 +15,19 @@ contract Todolist {
     // A mapping of the task id to the Task object that represents the todo list.
     mapping(uint => Task) public tasks;
 
+    // An event that represents the creation of a new task
+    event TaskCreated(
+        uint id,
+        string content,
+        bool completed
+    );
+
+    // An event that represents the completion of a task
+    event TaskCompleted(
+        uint id,
+        bool completed
+    );
+
     constructor() public {
         createTask("buy more ethereum!");
     }
@@ -25,5 +38,18 @@ contract Todolist {
         taskcount++;
         // Assign the new task to the tasks mapping
         tasks[taskcount] = Task(taskcount, _content, false);
+        // Emit a new TaskCreated event
+        emit TaskCreated(taskcount, _content, false);
+    }
+
+    function toggleCompleted(uint _id) public {
+        // Fetch the task from the mapping
+        Task memory _task = tasks[_id];
+        // Toggle its completion bool
+        _task.completed = !_task.completed;
+        // Reassign it to the mapping
+        tasks[_id] = _task; 
+        // Emit a new TaskCompleted event
+        emit TaskCompleted(_id, _task.completed);
     }
 }
